4ba1c50f234cc7ad582adad7f61c1b8bf9303449
Ian Healy <L00188608@atu.ie>
2026-03-22 13:22:59 +0000

    feat: add AddEditViewModel and shared application form partial

---
+++ Logbook/Views/Applications/_ApplicationForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Logbook.Views.Applications
+{
+    public class _ApplicationFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

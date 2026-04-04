b3ff9e9be272e6dd0dbff160e3a0fa32f9ac1eb8
Ian Healy <L00188608@atu.ie>
2026-04-04 12:55:29 +0100

    feat: add GetFilteredAsync method to service layer and interface

--- Logbook/Services/IJobApplicationService.cs
+++ Logbook/Services/IJobApplicationService.cs
@@ -6,6 +6,7 @@ public interface IJobApplicationService
 {
     Task<IEnumerable<JobApplication>> GetAllAsync();
     Task<IEnumerable<JobApplication>> GetByDateRangeAsync(DateTime from, DateTime to);
+    Task<IEnumerable<JobApplication>> GetFilteredAsync(ApplicationStatus? status, string? search);
     Task<JobApplication?> GetByIdAsync(int id);
     Task<JobApplication> AddAsync(JobApplication application);
     Task<JobApplication?> UpdateAsync(JobApplication application);
